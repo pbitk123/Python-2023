07225a1f32f19e9102da6fd91099e1731b475bad
pbit123 <pawel.bitkowski@asseco.pl>
2023-11-27 15:03:51 +0100

    Comment: zadanie z while

--- 05_Petle/while.py
+++ 05_Petle/while.py
@@ -58,3 +58,46 @@ while n > 0:
         print(n)
      else:
         print('Zla liczba')
+
+
+i=0
+while i<100:
+    i+=1
+    if i % 2 == 0 and (i % 10 + i // 10) % 7 == 0:
+        print(i)
+        continue
+else:
+    print('Koniec')
+
+
+suma=0
+i = input("podaj liczbe")
+for c in i:
+    suma += int(c)
+print(f'{suma}')
+
+
+liczba = input("Podaj liczbe")
+
+suma_liczb = 0
+for g in liczba:
+
+    suma_liczb += int(g)
+
+print(f"suma liczby{liczba} wynosi:{suma_liczb}")
+
+
+
+n = int(input("Podaj liczbę:"))
+print(f'Podałeś liczbę {n}')
+
+suma = 0
+while n > 0:
+    suma += n % 10
+    n //= 10
+
+print(suma)
+
+
+
+
